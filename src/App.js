91f49ddaba6c7b3ed57cc56cef5e4a31ab8eee63
Rakibul Islam <rakibulislammondal21@gmail.com>
2024-04-13 23:41:25 +0530

    Added Dark Mode Features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react';
 
 function App() {
+  const [mode,setMode] = useState("light");
+  
+  const modeChange = ()=>{
+    console.log("Button clicked");
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'gray';
+      document.body.style.color = '#fff';
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = '#fff';
+      document.body.style.color = '#212529';
+
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils"/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={modeChange} />
+
       <div className="container">
-        <TextForm heading="Enter text to analyze below" />
+        <TextForm heading="Enter text to analyze below" mode = {mode}/>
       </div>
+      {/* <About/> */}
     </>
   );
 }
