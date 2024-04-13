7cceb4f160ef415236bea944be9b6d68ecd3dff4
Rakibul Islam <rakibulislammondal21@gmail.com>
2024-04-13 23:43:54 +0530

    Modified for dark mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 export default function Navbar(props) {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme={`${props.mode}`}>
         <div className="container-fluid">
         <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,6 +21,10 @@ export default function Navbar(props) {
             </li>
             
           </ul>
+          <div className="form-check form-switch">
+            <input className="form-check-input mx-1" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <label className={`form-check-label mx-1 text-${props.mode==="light"?"dark" : "light"}`} htmlFor="flexSwitchCheckDefault">{props.mode} Mode</label>
+          </div>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success" type="submit">Search</button>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,25 +3,47 @@ import PropTypes from 'prop-types'
  
 export default function TextForm(props) {
     const [text,setText] = useState("Enter text here");
-
+    //Function to uppercase the text
     const handleUppercase = ()=>{
         console.log("Uppercse button was clicked");
         let newText = text.toUpperCase();
         setText(newText)
     }
-
+    //Function to lowercase the text
+    const handleLowercase = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    //Function to text to speech
+    const handleSpeech = ()=>{
+        let synth = window.speechSynthesis;
+        let voice = new SpeechSynthesisUtterance(text);
+        synth.speak(voice);
+    }
+    //Tracking change
     const changeUp = (e)=>{
         console.log("Clicked");
         // console.log(e.target.value);
         setText(e.target.value);
     }
    return (
-       <div className="mb-3">
+       <>
+            <div className="mb-3">
             <h1>{props.heading}</h1>
-            <textarea className="form-control" value={text} onChange={changeUp} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" data-bs-theme={`${props.mode}`} value={text} onChange={changeUp} id="myBox" rows="8"></textarea>
             {/*oneChange function helps to find the change on textarea*/}
-            <button className="btn btn-primary" onClick={handleUppercase}>Covert text to Uppercase</button>
+            <button className="btn btn-primary my-2" onClick={handleUppercase}>Covert text to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLowercase}>Convert Lowercase</button>
+            <button className="btn btn-success" onClick={handleSpeech}>Text-to-Speech</button>
         </div>
+        <div className="container">
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>Takes {0.008 * text.split(" ").length} minutes to Read</p>
+            <h2>Preview</h2>
+            <p>{text===""?"Enter text to show":text}</p>
+        </div>
+       </>
    )
 }
 
